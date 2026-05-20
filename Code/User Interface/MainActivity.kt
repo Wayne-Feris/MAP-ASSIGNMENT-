dc0e58bb5d5606c001e281834ca0d8531c8b45bd
Prata10 <hernaneprata05@gmail.com>
2026-05-20 17:57:34 +0200

    Update MainActivity.kt

--- Code/User Interface/MainActivity.kt
+++ Code/User Interface/MainActivity.kt
@@ -1,1 +1,36 @@
+package com.example.valentinesgarage.userinterface
 
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.valentinesgarage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnCheckIn.setOnClickListener {
+            startActivity(
+                Intent(this, CheckInActivity::class.java)
+            )
+        }
+
+        binding.btnRepairs.setOnClickListener {
+            startActivity(
+                Intent(this, RepairActivity::class.java)
+            )
+        }
+
+        binding.btnReports.setOnClickListener {
+            startActivity(
+                Intent(this, ReportActivity::class.java)
+            )
+        }
+    }
+}
