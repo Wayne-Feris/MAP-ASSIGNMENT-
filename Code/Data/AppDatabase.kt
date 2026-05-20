71190acead5ecd0be647476b6a64dcf501831b86
Immzgowa08 <immzgowa08@gmail.com>
2026-05-20 18:50:08 +0200

    Update AppDatabase.kt

--- Code/Data/AppDatabase.kt
+++ Code/Data/AppDatabase.kt
@@ -1,1 +1,36 @@
+package com.example.valentinesgarage.data
 
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(
+    entities = [Truck::class, Employee::class, RepairTask::class],
+    version = 1
+)
+abstract class AppDatabase : RoomDatabase() {
+
+    abstract fun garageDao(): GarageDao
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        fun getDatabase(context: Context): AppDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "garage_database"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+}
