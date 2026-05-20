3b6c4fc3e38483ba78fd1a57ceaa527da02bd77a
Jaydsz <jaydenhosh11@gmail.com>
2026-05-20 18:27:26 +0200

    Update GarageViewModel.kt

--- Code/ViewModel/GarageViewModel.kt
+++ Code/ViewModel/GarageViewModel.kt
@@ -1,1 +1,38 @@
+package com.example.valentinesgarage.viewmodel
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import com.example.valentinesgarage.data.*
+import com.example.valentinesgarage.repository.GarageRepository
+import kotlinx.coroutines.launch
+
+class GarageViewModel(application: Application) :
+    AndroidViewModel(application) {
+
+    private val repository: GarageRepository
+
+    val allTrucks: LiveData<List<Truck>>
+    val allTasks: LiveData<List<RepairTask>>
+
+    init {
+
+        val dao = AppDatabase.getDatabase(application).garageDao()
+
+        repository = GarageRepository(dao)
+
+        allTrucks = repository.allTrucks
+        allTasks = repository.allTasks
+    }
+
+    fun insertTruck(truck: Truck) =
+        viewModelScope.launch {
+            repository.insertTruck(truck)
+        }
+
+    fun insertTask(task: RepairTask) =
+        viewModelScope.launch {
+            repository.insertTask(task)
+        }
+    }
