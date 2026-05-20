5bb6a7050faaa6668c71be7a7dcbc737987ac800
Immzgowa08 <immzgowa08@gmail.com>
2026-05-20 18:54:24 +0200

    Update GarageDao.kt

--- Code/Data/GarageDao.kt
+++ Code/Data/GarageDao.kt
@@ -1,1 +1,14 @@
+package com.example.valentinesgarage.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "employees")
+data class Employee(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val name: String,
+    val role: String
+)
