81b02c2e90fcaf1b7521b6d21cf1e5b604c9867a
Prata10 <hernaneprata05@gmail.com>
2026-05-20 17:56:12 +0200

    Update ReportActivity.kt

--- Code/User Interface/ReportActivity.kt
+++ Code/User Interface/ReportActivity.kt
@@ -1,1 +1,30 @@
+package com.example.valentinesgarage.userinterface
 
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import com.example.valentinesgarage.databinding.ActivityReportBinding
+import com.example.valentinesgarage.viewmodel.GarageViewModel
+
+class ReportActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityReportBinding
+
+    private val viewModel: GarageViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityReportBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        viewModel.allTrucks.observe(this) { trucks ->
+
+            binding.tvReport.text = "Report Loaded"
+                trucks.joinToString("\n") {
+
+                    "${it.registrationNumber} - ${it.condition}"
+                }
+        }
+    }
+}
