a5a0796ce4d93fc6d18ca5876a381e3203e16752
Immzgowa08 <immzgowa08@gmail.com>
2026-05-20 18:54:56 +0200

    Update RepairTask.kt

--- Code/Data/RepairTask.kt
+++ Code/Data/RepairTask.kt
@@ -1,1 +1,17 @@
+package com.example.valentinesgarage.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "repair_tasks")
+data class RepairTask(
+
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val truckId: Int,
+    val employeeId: Int,
+    val taskName: String,
+    val notes: String,
+    val status: String
+)
