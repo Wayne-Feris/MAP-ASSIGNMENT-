4be23374df8cf2865fbdc8fa1f1cafe559b3e019
Jaydsz <jaydenhosh11@gmail.com>
2026-05-20 18:23:21 +0200

    Update Truck.kt

--- Code/Data/Truck.kt
+++ Code/Data/Truck.kt
@@ -1,1 +1,18 @@
+package com.example.valentinesgarage.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "trucks")
+data class Truck(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    val registrationNumber: String,
+    val ownerName: String,
+    val driverName: String,
+    val kilometers: Int,
+    val condition: String,
+    val fuelLevel: String,
+    val checkInDate: String
+)
